9e1dd252d6f90ed1f51c9f9a44875a837c541e6a
Milos Micevic <milos.micevic@asioso.com>
2023-05-15 19:19:36 +0200

    Adding preloader

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.scss";
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  setTimeout(() => {
+    preloader.classList.replace("opacity-100", "opacity-0");
+  }, 500);
+  setTimeout(() => {
+    preloader.classList.add("hidden");
+  }, 2000);
+});
